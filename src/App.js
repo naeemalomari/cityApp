56abc7ac75bb9370f4ed46c8e76af091e8d41f3b
naeemalomari <naeemalomari96@gmail.com>
2021-08-01 17:02:40 +0300

    class6

--- src/App.js
+++ src/App.js
@@ -4,28 +4,30 @@ import Button from 'react-bootstrap/Button';
 import axios from 'axios';
 
 class App extends React.Component {
-constructor(props){
-  super(props);
-  this.state={
-    display_name:'',
-    lat:'',
-    lon:'',
+  constructor(props) {
+    super(props);
+    this.state = {
+      display_name: '',
+      lat: '',
+      lon: '',
+      showMap: false,
+    }
   }
-}
 
-  getLocationData = async(event) => {
+  getLocationData = async (event) => {
     event.preventDefault();
     let cityName = event.target.city.value;
     console.log(cityName);
     // let key ='pk.63c388e715285390690165b87c5f6e49';
-    let URL= `https://eu1.locationiq.com/v1/search.php?key=pk.63c388e715285390690165b87c5f6e49&q=Empire State Building&format=json`
+    let URL = `https://eu1.locationiq.com/v1/search.php?key=pk.63c388e715285390690165b87c5f6e49&q=Empire State Building&format=json`
 
     let location = await axios.get(URL);
     // console.log(location.data[0].display_name)
     this.setState({
-      display_name:location.data[0].display_name,
+      display_name: location.data[0].display_name,
       lon: location.data[0].lon,
-      lat :location.data[0].lon,
+      lat: location.data[0].lon,
+      showMap: true
 
     })
   }
@@ -36,21 +38,25 @@ constructor(props){
         <Form onSubmit={this.getLocationData}>
           <Form.Group >
             <Form.Label  ></Form.Label>
-            <Form.Control  placeholder='Empire State Building Location ...' name='city' style={{width : "20rem"}} />
-            <Button type='submit ' style={{width:"8rem", height : "2rem" ,color :"blue"}}> Explore! </Button>
+            <Form.Control placeholder='Empire State Building Location ...' name='city' style={{ width: "20rem" }} />
+            <Button type='submit ' style={{ width: "8rem", height: "2rem", color: "blue" }}> Explore! </Button>
           </Form.Group>
         </Form>
         <p>
-          Area Name :   
-        {this.state.display_name}
+          Area Name :
+          {this.state.display_name}
         </p>
-        <p> longitude :   
-        ({this.state.lon})
+        <p> longitude :
+          ({this.state.lon})
         </p>
         <p>
-        Latitude :   
-        ({this.state.lat})
+          Latitude :
+          ({this.state.lat})
         </p>
+        {
+        this.state.showMap &&
+          < img src={`https://maps.locationiq.com/v3/staticmap?key=pk.63c388e715285390690165b87c5f6e49&center=${this.state.lat},${this.state.lon}zoom =18`} alt='map' />
+  }
       </>
     )
   }
