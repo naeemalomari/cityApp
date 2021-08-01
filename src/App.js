ea1b1f18d9856691c0f1108db5c5dc768e19ffcc
naeemalomari <naeemalomari96@gmail.com>
2021-08-01 15:58:36 +0300

    API servers

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+constructor(props){
+  super(props);
+  this.state={
+    display_name:'',
+    lat:'',
+    lon:'',
+  }
 }
 
+  getLocationData = async(event) => {
+    event.preventDefault();
+    let cityName = event.target.city.value;
+    console.log(cityName);
+    // let key ='pk.63c388e715285390690165b87c5f6e49';
+    let URL= `https://eu1.locationiq.com/v1/search.php?key=pk.63c388e715285390690165b87c5f6e49&q=Empire State Building&format=json`
+
+    let location = await axios.get(URL);
+    // console.log(location.data[0].display_name)
+    this.setState({
+      display_name:location.data[0].display_name,
+      lon: location.data[0].lon,
+      lat :location.data[0].lon,
+
+    })
+  }
+  render() {
+    return (
+      <>
+        <h1> City Explorer</h1>
+        <Form onSubmit={this.getLocationData}>
+          <Form.Group >
+            <Form.Label  ></Form.Label>
+            <Form.Control  placeholder='Empire State Building Location ...' name='city' style={{width : "20rem"}} />
+            <Button type='submit ' style={{width:"8rem", height : "2rem" ,color :"blue"}}> Explore! </Button>
+          </Form.Group>
+        </Form>
+        <p>
+          Area Name :   
+        {this.state.display_name}
+        </p>
+        <p> longitude :   
+        ({this.state.lon})
+        </p>
+        <p>
+        Latitude :   
+        ({this.state.lat})
+        </p>
+      </>
+    )
+  }
+}
 export default App;
